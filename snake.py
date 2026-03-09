6c4cda504f8451e57f3023e3f65aaac54e948c8a
migueljose28 <miguelneto275@gmail.com>
2026-03-08 21:06:03 -0300

    validation move direction

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20,0), (-40,0)]
 MOVE_DISTANCE = 20
+RIGHT, UP, LEFT, DOWN = 0, 90, 180, 270
 
 
 
@@ -8,7 +9,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
-
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -23,4 +24,20 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
